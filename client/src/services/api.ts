74f4828942a90e49497fb6a8a3feb50bbf8b01f2
shelcia <shelcia1999@gmail.com>
2024-06-19 12:48:24 -0500

    🚀 switch to render

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,4 +1,3 @@
-export const BACKEND_URL: string =
-  "https://skin-tone-rating-backend.vercel.app/api";
+export const BACKEND_URL: string = "https://skin-tone-rating.onrender.com/api";
 
 export const LOCALHOST_URL: string = "http://localhost:4050/api";
